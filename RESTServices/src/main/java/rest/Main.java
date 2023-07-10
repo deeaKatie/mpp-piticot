58705284c1a16db23c30bf1e533865eb18569fb0
Deea <deea.lip@gmail.com>
2023-07-10 13:13:39 +0300

    base app

--- RESTServices/src/main/java/rest/Main.java
+++ RESTServices/src/main/java/rest/Main.java
@@ -9,20 +9,8 @@ import org.springframework.context.annotation.ComponentScan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        SpringApplication.run(Main.class, args); // how he know?
+        SpringApplication.run(Main.class, args);
     }
 
-//    @Bean(name="props")
-//    @Primary
-//    public Properties getBdProperties(){
-//        Properties props = new Properties();
-//        try {
-//            System.out.println("Searching bd.config in directory "+((new File(".")).getAbsolutePath()));
-//            props.load(new FileReader("db.config"));
-//        } catch (IOException e) {
-//            System.err.println("Configuration file bd.cong not found" + e);
-//
-//        }
-//        return props;
-//    }
+
 }
